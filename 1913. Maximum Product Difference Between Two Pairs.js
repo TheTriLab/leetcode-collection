8f7ac7583918492266f4b714e795fe8e5df8b1cd
triduong <mtworkweb@gmail.com>
2024-04-05 23:43:10 +0700

    1913. Maximum Product Difference Between Two Pairs

---
+++ 1913. Maximum Product Difference Between Two Pairs.js
@@ -0,0 +1,11 @@
+// Challenge https://leetcode.com/problems/maximum-product-difference-between-two-pairs/description/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProductDifference = function(nums) {
+    nums.sort((a, b) => a - b)
+
+    return (nums[nums.length - 1] * nums[nums.length - 2]) - (nums[0] * nums[1])
+};
