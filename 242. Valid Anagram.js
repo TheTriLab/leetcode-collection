50bd258b574287e6323cc113e26df39e05f3a692
triduong <mtworkweb@gmail.com>
2024-05-11 23:58:22 +0700

    242. Valid Anagram

---
+++ 242. Valid Anagram.js
@@ -0,0 +1,28 @@
+// Challenge https://leetcode.com/problems/valid-anagram/description/
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    const sMap = new Map()
+    const tMap = new Map()
+    let res = true
+
+    for (let i = 0; i < s.length; i++) {
+        sMap.set(s[i], (sMap.get(s[i]) || 0) + 1)
+    }
+
+    for (let j = 0; j < t.length; j++) {
+        tMap.set(t[j], (tMap.get(t[j]) || 0) + 1)
+    }
+
+    sMap.forEach((value, key) => {
+        if (value !== tMap.get(key)) {
+            res = false
+        }
+    })
+
+    return res
+};
