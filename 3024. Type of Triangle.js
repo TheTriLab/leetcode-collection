d3e754a2b0fa6a28e23c7d4cd9671176e8da99ac
triduong <mtworkweb@gmail.com>
2024-07-03 23:56:49 +0700

    3024. Type of Triangle

---
+++ 3024. Type of Triangle.js
@@ -0,0 +1,27 @@
+// Challenge https://leetcode.com/problems/type-of-triangle/description/
+
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var triangleType = function(nums) {
+    nums.sort((a, b) => a - b)
+
+    if ((nums[0] + nums[1]) <= nums[2]) {
+        return "none"
+    }
+
+    if (nums[0] === nums[1] && nums[1] === nums[2]) {
+        return "equilateral"
+    }
+
+    if (
+        nums[0] === nums[1] 
+        || nums[0] === nums[2] 
+        || nums[1] === nums[2]
+    ) {
+        return "isosceles"
+    }
+
+    return "scalene"
+};
